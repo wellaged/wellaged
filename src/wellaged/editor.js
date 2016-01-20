bd1947807cfb7e9802ad7f05ca481e5ad4608b90
Andreas C. Osowski <andreas@osowski.de>
2016-01-20 12:32:28 +0100

    Further refactoring Removed events from EditorView Removed toolbar from EditorView Moved toolbar CSS to index.html

--- src/wellaged/editor.js
+++ src/wellaged/editor.js
@@ -4,64 +4,16 @@ import * as jsyaml from 'js-yaml';
 import _ from 'lodash';
 
 import Factory from './factory';
-import {
-    Selection, SelectionView
-}
-from './selection';
 
 var EditorView = Backbone.View.extend({
-    events: {
-        'click .wellaged-toolbar .add-issue': 'addIssue',
-        'click .wellaged-toolbar .add-argument': 'addArgument',
-        'click .wellaged-toolbar .add-statement': 'addStatement',
-        'click .wellaged-toolbar .preview-dialog': 'previewDialog',
-        'click .wellaged-toolbar .to-yaml': 'graphToYAML',
-        'click .wellaged-toolbar .from-yaml': 'graphFromYAML',
-        'click .wellaged-toolbar .carneades': 'runCarneades',
-        'click .wellaged-toolbar .clear': 'clear',
-        'click .wellaged-toolbar .auto-layout': 'doAutoLayout'
-    },
-
     initialize: function() {
         this.initializePaper();
-        this.initializeSelection();
-    },
-
-    initializeSelection: function() {
-
-        document.body.addEventListener('keydown', (evt) => {
-            var code = evt.which || evt.keyCode;
-            if (code === 8 || code === 46) {
-                // backspace or delete
-
-                return false;
-            }
-            return true;
-        });
-
-        var selection = this.selection = new Selection();
-        var selectionView = new SelectionView({
-            model: selection,
-            paper: this.paper
-        });
-
-        this.paper.on('cell:pointerup', (cellView) => {
-
-            if (!cellView.model.isLink()) {
-                selection.reset([cellView.model]);
-            }
-        });
-        this.paper.on('blank:pointerdown', function() {
-            selection.reset([]);
-        });
-
-        selection.on('add reset', this.onSelectionChange, this);
     },
 
     initializePaper: function() {
 
         this.graph = new joint.dia.Graph();
-      
+
         this.paper = new joint.dia.Paper({
             el: this.$('.wellaged-paper'),
             model: this.graph,
@@ -104,34 +56,30 @@ var EditorView = Backbone.View.extend({
         });
     },
 
-    onSelectionChange: function(collection) {
-        var cell = collection.first();
+    addIssue: function(label) {
+        let i = Factory.createIssue(label);
+        this.graph.addCell(i);
+        return i;
     },
 
-    addIssue: function() {
-
-        var q = Factory.createIssue('i3: PE');
-        this.graph.addCell(q);
-    },
-
-    addArgument: function() {
-        var a = Factory.createArgument('Argument.');
+    addArgument: function(label) {
+        let a = Factory.createArgument(label);
         this.graph.addCell(a);
+        return a;
     },
 
-    addStatement: function() {
-        let a = Factory.createStatement('Statement');
-        this.graph.addCell(a);
+    addStatement: function(label, assumed) {
+        let s = Factory.createStatement(label, assumed);
+        this.graph.addCell(s);
+        return s;
     },
 
     graphToYAML: function() {
-        console.dir(this.graph);
         let yaml = Factory.createYAML(this.graph);
-
-        console.log(yaml);
+        return yaml;
     },
 
-    graphFromYAML: function() {
+    graphFromYAML: function(yamlTxt) {
         const helper = (source, target) =>
             new joint.shapes.devs.Link({
                 source: {
@@ -144,36 +92,32 @@ var EditorView = Backbone.View.extend({
                 }
             });
 
-        $.get("/porsche.yml", (yamlTxt) => {
-            const yaml = jsyaml.load(yamlTxt);
+        const yaml = jsyaml.load(yamlTxt);
 
-            this.clear();
+        this.clear();
 
-            _.each(yaml.statements, (stmt, id) => {
-                const s = Factory.createStatement(stmt, id, _.includes(yaml.assumptions, id));
-                this.graph.addCell(s);
-            });
-
-            _.each(yaml.arguments, (arg, id) => {
-                const a = Factory.createArgument(id, id);
-                this.graph.addCell(a);
-                this.graph.addCell(helper(id, arg.conclusion));
-                for (let premise of arg.premises) this.graph.addCell(helper(premise, id));
-            });
-
-            _.each(yaml.issues, (issue, id) => {
-                const i = Factory.createIssue(id, id);
-
-                this.graph.addCell(i);
-
-                for (let pos of issue.positions) this.graph.addCell(helper(pos, id));
-
-            });
-
-            this.doAutoLayout();
+        _.each(yaml.statements, (stmt, id) => {
+            const s = Factory.createStatement(stmt, id, _.includes(yaml.assumptions, id));
+            this.graph.addCell(s);
         });
 
+        _.each(yaml.arguments, (arg, id) => {
+            const a = Factory.createArgument(id, id);
+            this.graph.addCell(a);
+            this.graph.addCell(helper(id, arg.conclusion));
+            for (let premise of arg.premises) this.graph.addCell(helper(premise, id));
+        });
 
+        _.each(yaml.issues, (issue, id) => {
+            const i = Factory.createIssue(id, id);
+
+            this.graph.addCell(i);
+
+            for (let pos of issue.positions) this.graph.addCell(helper(pos, id));
+
+        });
+
+        this.doAutoLayout();
     },
 
     doAutoLayout: function() {
@@ -208,4 +152,6 @@ var EditorView = Backbone.View.extend({
     }
 });
 
-export {EditorView};
+export {
+    EditorView
+};
