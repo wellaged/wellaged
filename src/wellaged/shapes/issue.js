f39edc80db2aaaed2b092ce25fd3edff19a10709
Andreas C. Osowski <andreas@osowski.de>
2016-01-27 13:02:16 +0100

    Fixed regression regarding Issue shape Increased roundness of Argument corners

--- src/wellaged/shapes/issue.js
+++ src/wellaged/shapes/issue.js
@@ -9,6 +9,7 @@ joint.shapes.wellaged = joint.shapes.wellaged || {};
 
 joint.shapes.wellaged.Issue = joint.shapes.wellaged.Basic.extend({
   defaults: joint.util.deepSupplement({
+    markup: '<g class="rotatable"><g class="scalable"> <g class="body" transform="translate(-370.987, 215.46)"> <polygon points="475.013,-194 449.006,-215.46 396.994,-215.46 370.987,-194 396.994,-172.54 449.006,-172.54 475.013,-194" /></g></g><text/><g class="inPorts"/><g class="outPorts"/></g>',
 
     type: 'wellaged.Issue',
     size: {
